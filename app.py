b04cccdc0593f50fbd729a8ecc3e8c9fd6d10a37
sedego <sedego@gmail.com>
2020-09-23 02:01:32 -0400

    added CRUD fonctionality

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ class toDoApp(db.Model):
 @app.route('/')
 def index():
     incomplete = toDoApp.query.filter_by(complete=False).all()
-    return render_template('index.html', incomplete=incomplete)
+    complete = toDoApp.query.filter_by(complete=True).all()
+    return render_template('index.html', incomplete=incomplete, complete=complete)
 
 @app.route('/add', methods=['POST'])
 def add():
@@ -23,5 +24,27 @@ def add():
     db.session.add(dbApp)
     db.session.commit()
     return redirect(url_for('index'))
+
+@app.route('/complete/<id>')
+def complete(id):
+    dbApp = toDoApp.query.filter_by(id=int(id)).first()
+    dbApp.complete = True
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/uncheck/<id>')
+def uncheck(id):
+    dbApp = toDoApp.query.filter_by(id=int(id)).first()
+    dbApp.complete = False
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<id>')
+def delete(id):
+    dbApp = toDoApp.query.filter_by(id=int(id)).first()
+    db.session.delete(dbApp)
+    db.session.commit()
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(debug=True)
